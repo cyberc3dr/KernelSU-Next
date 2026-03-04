72504b2f0f6be6ae6551c51a4f76e51da3469e0f
cyberc3dr <geo3gamer@gmail.com>
2026-03-04 14:20:49 +0300

    fix: Hook mode names from Wild KSU

--- kernel/supercalls.c
+++ kernel/supercalls.c
@@ -539,10 +539,10 @@ static int do_get_hook_mode(void __user *arg)
 {
 	struct ksu_get_hook_mode_cmd cmd = {0};
 
-#ifndef CONFIG_KSU_SUSFS
+#if !defined(CONFIG_KSU_SUSFS) && defined(CONFIG_KPROBES)
 	strscpy(cmd.mode, "Kprobes", sizeof(cmd.mode));
-#elif defined(CONFIG_KPROBES) || defined(CONFIG_TRACEPOINTS)
-	strscpy(cmd.mode, "Hybrid", sizeof(cmd.mode));
+#elif defined(CONFIG_KSU_SUSFS)
+	strscpy(cmd.mode, "Inline (SUSFS)", sizeof(cmd.mode));
 #else
 	strscpy(cmd.mode, "Inline", sizeof(cmd.mode));
 #endif // CONFIG_KSU_SUSFS
